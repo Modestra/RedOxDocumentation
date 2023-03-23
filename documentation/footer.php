fe0392ae8b6dd38591e9a1afdcc3d1a959d3bfe0
Kirill <104451159+Modestra@users.noreply.github.com>
2023-03-23 23:52:49 +0700

    Создание основы сайта

---
+++ documentation/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    Подвал
+</footer>
+</body>
+</html>
---
+++ documentation/index.php
@@ -0,0 +1,6 @@
+<?php require("header.php")?>
+<main class="main-content-block">
+    Основа
+</main>
+<?php require("footer.php")?>
+
