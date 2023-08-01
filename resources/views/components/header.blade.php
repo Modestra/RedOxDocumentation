1563c943551e54ca99f600b6f2d4f780dcf728dd
Kirill <104451159+Modestra@users.noreply.github.com>
2023-08-01 20:43:37 +0700

    Подключерие Laravel/Создание основных компонентов

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<header style="height: 50px" class="header-title">
+    <div class="container-xxl">
+        <div class="row">
+            <div class="col-sm-1" style="width: 50px; height: 50px"><img src="..."></div>
+            <div class="row col">
+                <div class="col-sm-auto"><p>RedOx</p></div>
+                <div class="col-sm-auto"><a href="/documentation">Документация</a></div>
+                <div class="col-sm-auto"><a href="/products">Продукты</a></div>
+            </div>
+            <div class="col-sm-auto"><input class="form-control" placeholder="Поиск по странице"></div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/instructionstart.blade.php
@@ -0,0 +1,12 @@
+@include("components.header")
+<div class="container text-start documentation">
+    <div class="row row-cols-3 documentation-col">
+        <section class="col-sm-2 text-start doc-nav">
+            <ul class="doc-nav-title"><h5>Начало работы</h5></ul>
+        </section>
+        <div class="col-8 doc-list text-start">
+            <h4>Начало работы</h4>
+        </div>
+        <div class="col-1 doc-info"></div>
+    </div>
+</div>
